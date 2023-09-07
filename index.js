6bf966718df523d2e1bf18d56576efa99e6de75a
VladimirVolvakov <dr.volvakov@gmail.com>
2023-09-07 23:47:25 +0300

    style: implement order summary auto-filling functionality when selecting order options in section.create-plan in create-plan.html

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ let openBtn2 = document.querySelector('#open-options-2');
 let openBtn3 = document.querySelector('#open-options-3');
 let openBtn4 = document.querySelector('#open-options-4');
 let openBtn5 = document.querySelector('#open-options-5');
+
 let optionsSet1 = document.querySelector('#options-set-1');
 let optionsSet2 = document.querySelector('#options-set-2');
 let optionsSet3 = document.querySelector('#options-set-3');
 let optionsSet4 = document.querySelector('#options-set-4');
 let optionsSet5 = document.querySelector('#options-set-5');
+
+let form = document.querySelector('.create-plan__form');
+let submitBtn = document.querySelector('#submit-btn');
+
+let coffeeForm = document.getElementsByName('coffee-form');
+let coffeeType = document.getElementsByName('coffee-type');
+let coffeeQuantity = document.getElementsByName('coffee-qty');
+let coffeeGrindType = document.getElementsByName('coffee-grind');
+let deliveryFrequency = document.getElementsByName('delivery-frequency');
+
+let coffeeFormContent = document.querySelector('#summary-option-1');
+let coffeeTypeContent = document.querySelector('#summary-option-2');
+let coffeeQuantityContent = document.querySelector('#summary-option-3');
+let coffeeGrindTypeContent = document.querySelector('#summary-option-4');
+let deliveryFrequencyContent = document.querySelector('#summary-option-5');
 
 openBtn1.addEventListener('click', () => {
   openBtn1.classList.toggle('create-plan__btn-opened-options');
@@ -33,3 +49,48 @@ openBtn5.addEventListener('click', () => {
   openBtn5.classList.toggle('create-plan__btn-opened-options');
   optionsSet5.classList.toggle('create-plan__options-open');
 });
+
+function getFormFieldData (options) {
+  let optionValue;
+
+  for (let i = 0; i < options.length; i++) {
+    if (options[i].checked) {
+      optionValue = options[i].value;
+      break;
+    }
+  }
+
+  return optionValue;
+}
+
+optionsSet1.addEventListener('change', () => {
+  let coffeeFormValue = getFormFieldData(coffeeForm);
+  coffeeFormContent.innerHTML = coffeeFormValue;
+});
+
+optionsSet2.addEventListener('change', () => {
+  let coffeeTypeValue = getFormFieldData(coffeeType);
+  coffeeTypeContent.innerHTML = coffeeTypeValue;
+});
+
+optionsSet3.addEventListener('change', () => {
+  let coffeeQuantityValue = getFormFieldData(coffeeQuantity);
+  coffeeQuantityContent.innerHTML = coffeeQuantityValue;
+});
+
+optionsSet4.addEventListener('change', () => {
+  let coffeeGrindTypeValue = getFormFieldData(coffeeGrindType);
+  coffeeGrindTypeContent.innerHTML = coffeeGrindTypeValue;
+});
+
+optionsSet5.addEventListener('change', () => {
+  let deliveryFrequencyValue = getFormFieldData(deliveryFrequency);
+  deliveryFrequencyContent.innerHTML = deliveryFrequencyValue;
+});
+
+function onFormSubmit (event) {
+  event.preventDefault();
+  console.log('Form is submitted');
+}
+
+form.addEventListener('submit', onFormSubmit);
